2e73d4ddc727009750a408ca63ee36b862540ed5
receiptdev <receipt.defy@me.com>
2018-08-21 16:48:40 +0900

    Beautiful Binary String(did not done, keep working)

--- Algorithms/Strings/StrongPassword.js
+++ Algorithms/Strings/StrongPassword.js
@@ -1,38 +1,43 @@
 function minimumNumber(n, password) {
-  const specialChar = '!@#$%^&*()-+';
-  const passwordFields = password.split('');
-  const flag = {
-    number: false,
-    lowerCase: false,
-    upperCase: false,
-    specialChar: false,
-  };
-  let enhanced = 0;
+    const specialChar = "!@#$%^&*()-+";
+    const passwordFields = password.split("");
+    let flag = {
+        number: false,
+        lowerCase: true,
+        upperCase: false,
+        special: false
+    };
+    let { number, lowerCase, upperCase, special } = flag;
+    let enhanced = 0;
 
-  passwordFields.map(passwordField => {
-    if (passwordField >= '0' && passwordField <= '9') {
-      flag.number = true;
-    }
-    if (passwordField >= 'a' && passwordField <= 'z') {
-      flag.lowerCase = true;
-    }
-    if (passwordField >= 'A' && passwordField <= 'Z') {
-      flag.upperCase = true;
-    }
-    if (specialChar.indexOf(passwordField) >= 0) {
-      flag.specialChar = true;
-    }
-  });
+    console.log(number, lowerCase, upperCase, special);
 
-  for (const key of Object.keys(flag)) {
-    if (!flag[key]) {
-      enhanced++;
+    passwordFields.map(passwordField => {
+        if (passwordField >= "0" && passwordField <= "9") {
+            number = true;
+        }
+        if (passwordField >= "a" && passwordField <= "z") {
+            lowerCase = true;
+        }
+        if (passwordField >= "A" && passwordField <= "Z") {
+            upperCase = true;
+        }
+        if (specialChar.indexOf(passwordField) >= 0) {
+            special = true;
+        }
+    });
+
+    for (const key of Object.keys(flag)) {
+        if (!flag[key]) {
+            enhanced++;
+        }
     }
-  }
 
-  if (n + enhanced < 6) {
-    enhanced += 6 - (n + enhanced);
-  }
+    if (n + enhanced < 6) {
+        enhanced += 6 - (n + enhanced);
+    }
 
-  return enhanced;
+    console.log("enhanced", enhanced);
+
+    return enhanced;
 }
